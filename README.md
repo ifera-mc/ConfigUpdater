d5196d8ffd599e03e338420b1b03847facf919eb
JackMD <jackmtaylor.jmt@gmail.com>
2019-02-12 01:07:51 +0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # ConfigUpdater
-A handy virion for PocketMine-MP plugin developers that checks if a new version of config is available. If so then it notifies the user about the new update and updates the config.
+
+| HitCount | License |
+|:--:|:--:|
+|[![HitCount](http://hits.dwyl.io/JackMD/ConfigUpdater.svg)](http://hits.dwyl.io/JackMD/ConfigUpdater)|[![GitHub license](https://img.shields.io/github/license/JackMD/ConfigUpdater.svg)](https://github.com/JackMD/ConfigUpdater/blob/master/LICENSE)|
+
+### A handy virion for PocketMine-MP plugin developers that checks if a new version of config is available. If so then it notifies the user about the new update and updates the config.
+
+### Features
+
+- Super simple virion to be used in your plugins.
+- It checks if a new release for a plugin is available on Poggit. If so then it notifies the user about the new release.
+
+### API
+
+```php
+    JackMD\ConfigUpdater\ConfigUpdater::checkUpdate(Plugin $plugin, Config $config, string $configPath, string $configName, string $configExtension, string $configKey, int $latestVersion, string $updateMessage);
+```
+<br />
+- `$plugin` is the plugin whom you are calling the function from.
+- `$config` is the config you want to update.
+- `$configPath` if the config is located in some other folder in the resources folder then use this to add the folder.
+- `$configName` is the name of the config without its extension.
+- `$configExtension` is the extension of the config without the dot.
+- `$configKey` is the version key that needs to be checked in the config.
+- `$latestVersion` is the latest version of the config. Needs to be integer.
+- `$updateMessage` is the update message that would be shown on console if the plugin is outdated.
+<br />
+<br />
+- For information regarding how to use a virion in a plugin please refer [here](https://poggit.github.io/support/virion.html)
+
+### Disclaimer
+
+This plugin is designed to be used only by PocketMine-MP developers who wish to provide their users with the info of when an update to the config is available.
---
+++ src/JackMD/ConfigUpdater/ConfigUpdater.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types = 1);
+
+/**
+ *  _____              __ _       _   _           _       _
+ * /  __ \            / _(_)     | | | |         | |     | |
+ * | /  \/ ___  _ __ | |_ _  __ _| | | |_ __   __| | __ _| |_ ___ _ __
+ * | |    / _ \| '_ \|  _| |/ _` | | | | '_ \ / _` |/ _` | __/ _ \ '__|
+ * | \__/\ (_) | | | | | | | (_| | |_| | |_) | (_| | (_| | ||  __/ |
+ *  \____/\___/|_| |_|_| |_|\__, |\___/| .__/ \__,_|\__,_|\__\___|_|
+ *                           __/ |     | |
+ *                          |___/      |_|
+ *
+ * ConfigUpdater, a updater virion for PocketMine-MP
+ * Copyright (c) 2018 JackMD  < https://github.com/JackMD >
+ *
+ * Discord: JackMD#3717
+ * Twitter: JackMTaylor_
+ *
+ * This software is distributed under "GNU General Public License v3.0".
+ *
+ * ConfigUpdater is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License v3.0 for more details.
+ *
+ * You should have received a copy of the GNU General Public License v3.0
+ * along with this program. If not, see
+ * <https://opensource.org/licenses/GPL-3.0>.
+ * ------------------------------------------------------------------------
+ */
+
+namespace JackMD\ConfigUpdater;
+
+use pocketmine\plugin\Plugin;
+use pocketmine\utils\Config;
+
+class ConfigUpdater{
+
+	/**
+	 * @param Plugin $plugin          The plugin you are calling this from.
+	 * @param Config $config          The config you want to update.
+	 * @param string $configPath      If the config is located in some other folder in the resources folder then use this to add the folder.
+	 * @param string $configName      The name of the config without its extension.
+	 * @param string $configExtension The extension of the config without the dot.
+	 * @param string $configKey       The version key that needs to be checked in the config.
+	 * @param int    $latestVersion   The latest version of the config. Needs to be integer.
+	 * @param string $updateMessage   The update message that would be shown on console if the plugin is outdated.
+	 */
+	public static function checkUpdate(Plugin $plugin, Config $config, string $configPath, string $configName, string $configExtension, string $configKey, int $latestVersion, string $updateMessage){
+		if(($config->exists($configKey)) || ((int) $config->get($configKey) === $latestVersion)){
+			return;
+		}
+
+		rename($plugin->getDataFolder() . $configPath . $configName . "." . $configExtension, $plugin->getDataFolder() . $configPath . $configName . "_old." . $configExtension);
+
+		$plugin->saveResource($configPath . $configName . "." . $configExtension);
+		$plugin->getLogger()->critical($updateMessage);
+	}
+}
