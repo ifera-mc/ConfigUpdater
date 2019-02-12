36215c7d9aa5b63b84a6f3cd21b062abc34c084f
Jack M. Taylor <32965703+JackMD@users.noreply.github.com>
2019-02-12 21:53:54 +0500

    Update ConfigUpdater.php

--- src/JackMD/ConfigUpdater/ConfigUpdater.php
+++ src/JackMD/ConfigUpdater/ConfigUpdater.php
@@ -53,7 +53,7 @@ class ConfigUpdater{
 			return;
 		}
 		if(trim($updateMessage) === ""){
-			$updateMessage = "Your " . $configName . "." . $configExtension . " file is outdated. Your old " . $configName . "." . $configExtension . " has been saved as " . $configName . "_old." . $configExtensionand . " and a new " . $configName . "." . $configExtension . " file has been generated. Please update accordingly.";
+			$updateMessage = "Your " . $configName . "." . $configExtension . " file is outdated. Your old " . $configName . "." . $configExtension . " has been saved as " . $configName . "_old." . $configExtension . " and a new " . $configName . "." . $configExtension . " file has been generated. Please update accordingly.";
 		}
 
 		rename($plugin->getDataFolder() . $configPath . $configName . "." . $configExtension, $plugin->getDataFolder() . $configPath . $configName . "_old." . $configExtension);
